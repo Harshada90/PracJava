a02d406bdf9aaca6e34c5f228a743e5db84fe7d2
Ganesh <Ganesh@LAPTOP-13GNM0MV>
2022-08-25 13:58:34 +0530

    MasterBranch

---
+++ PracJava/src/prac/MyCode.java
@@ -0,0 +1,16 @@
+package prac;
+
+public class MyCode {
+
+	public static void main(String args[]) {
+		for(int i=1;i<=10;i++)
+			System.out.println(i*2-1);
+		
+		
+		for(int i=1;i<=20;i++)
+			if(i%2!=0)
+			System.out.println("odd no :"+i);
+			else
+				System.out.println("");
+	}
+}
---
+++ PracJava/src/prac/PracCode.java
@@ -0,0 +1,7 @@
+package prac;
+
+public class PracCode {
+ public static void main(String[] args) {
+	System.out.println("Good morning");
+}
+}
